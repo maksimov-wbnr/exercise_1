79f0ca0d3b99c94701ababd9ac0588ae9ddf4611
Andrey Maximov <a.maksimov@webinar.ru>
2022-12-09 20:38:41 +0300

    Создал проект для idea

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -1,6 +1,6 @@
 public class FirstProgram{
     public static void main(String[] args) {
-            System.out.println("Hello world");
+            System.out.println("Моё первое задание");
         }
     }
 
