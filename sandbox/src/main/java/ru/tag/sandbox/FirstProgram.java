24ba03280b820aa5ac40c5127abd7e1efc960252
Andrey Maximov <a.maksimov@webinar.ru>
2022-12-11 19:00:16 +0300

    добавлено пара функций

--- sandbox/src/main/java/ru/tag/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/tag/sandbox/FirstProgram.java
@@ -1,9 +1,38 @@
 package ru.tag.sandbox;
 
+import java.sql.SQLOutput;
+
 public class FirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Моё первое задание!");
+        hello("world");
+        hello("user");
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника с сторонами "+ a +" и "+ b +" = "+ area(a,b));
+
+
     }
-}
+
+        public static void hello (String somebody){
+            System.out.println("Hello " + somebody);
+        }
+
+        public static double area ( double l){
+            return l * l;
+        }
+
+        public static double area ( double a, double b){
+          return a*b;
+        }
+
+
+    }
+
+
+
+
 
