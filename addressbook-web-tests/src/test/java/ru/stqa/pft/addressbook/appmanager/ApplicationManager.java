820551b86bccbd9eedf55e21ce6a2b8430826f81
Andrey Maximov <a.maksimov@webinar.ru>
2022-12-25 10:55:57 +0300

    создан помощник SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -13,6 +13,7 @@ public class ApplicationManager {
 
   private  NavigationHelper navigationHelper;
   private  GroupHelper groupHelper;
+  private SessionHelper sessionHelper;
 
   public void init() {
     wd = new FirefoxDriver();
@@ -20,17 +21,11 @@ public class ApplicationManager {
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
+    sessionHelper =new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
-  public void login(String username, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+
 
   public void stop() {
     wd.quit();
