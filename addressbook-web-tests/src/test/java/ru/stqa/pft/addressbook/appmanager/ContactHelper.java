49237fb584016114a9987a80446fa4d30a3a5902
Andrey Maximov <a.maksimov@webinar.ru>
2022-12-28 18:22:25 +0300

    Добавлен тестовый класс ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -26,13 +26,19 @@ public class ContactHelper extends HelperBase {
   }
 
   public void selectContact() {
-    click(By.xpath("//*[@id='31']"));
+    click(By.xpath("//*[@id='2']"));
   }
   public void deleteSelectedContacts() {
     click(By.xpath("//input[@value='Delete']"));
   }
   public void alertAccept(){
     wd.switchTo().alert().accept();
+  }
+  public void initContactModification() {
+    click(By.xpath("//td[8]/a/img"));
+  }
+  public void submitContactModification() {
+    click(By.xpath("//input[22]"));
   }
 
 }
