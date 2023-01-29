6e9acd256a2e7dd38c07079344ee9a4cfb1f40b9
Andrey Maximov <a.maksimov@webinar.ru>
2023-01-29 18:59:56 +0300

    5.6 для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
   String GroupName = "test2";
@@ -41,6 +43,7 @@ public class ContactCreationTests extends TestBase {
     contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt());
     before.add(contact);
     Assert.assertEquals(before, after);
+    
   }
 
 }
