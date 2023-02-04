f1271cc38b85560cb0050b0084f8a08a389514c5
Andrey Maximov <a.maksimov@webinar.ru>
2023-02-04 15:03:55 +0300

    6.5
    для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,15 +1,40 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
   String GroupName = "test2";
+
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().withLastName(split[0]).withFirstName(split[1]).withAddress(split[2])
+              .withEmail(split[3]).withMobilePhone(split[4]).withGroup(GroupName)});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
 
   @BeforeMethod
   public void ensurePreconditions() {
@@ -20,24 +45,10 @@ public class ContactCreationTests extends TestBase {
     app.goTo().homePage();
   }
 
-  @Test
-  public void testAddNewContact() {
-
+  @Test(dataProvider = "validContacts")
+  public void testAddNewContact(ContactData contact) {
     Contacts before = app.contact().all();
     app.contact().addNew();
-    ContactData contact = new ContactData()
-            .withFirstName("First")
-            .withMiddleName("Middle")
-            .withLastName("Last")
-            .withHomePhone("919191")
-            .withMobilePhone("89111111111")
-            .withWorkPhone("900")
-            .withEmail("1@tag.ru")
-            .withEmail2("2@tag.ru")
-            .withEmail3("3@tag.ru")
-            .withGroup(GroupName)
-            .withAddress("Address st. 52")
-            .withPhone2("909090");
     app.contact().fillContactForm(contact, true);
     app.contact().submitContactForm();
     app.goTo().homePage();
