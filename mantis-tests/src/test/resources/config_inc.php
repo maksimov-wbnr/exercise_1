1e371845d37c1c9d4c288490752da88902765ee3
Andrey Maximov <a.maksimov@webinar.ru>
2023-02-21 10:03:22 +0300

    8.6

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -4,8 +4,8 @@ $g_db_type                = 'mysqli';
 $g_database_name          = 'bugtracker';
 $g_db_username            = 'root';
 $g_db_password            = '';
-
 $g_default_timezone       = 'Europe/Berlin';
-
 $g_crypto_master_salt     = 'o8nbjtAOiSP8iYV2ZLnMwV//swYPRUJvwxBqZ6NMUNc=';
 $g_signup_use_captcha     = OFF;
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
+$g_smtp_host              = 'localhost';
