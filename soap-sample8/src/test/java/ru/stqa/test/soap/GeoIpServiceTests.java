de18039c5f9cf116c3b98881718b7789b9624a68
Andrey Maximov <a.maksimov@webinar.ru>
2023-02-28 16:13:24 +0300

    9.2
    Добавил JAVA_HOME в сист настрйоки, убрал путь к 16 JAVA

---
+++ soap-sample8/src/test/java/ru/stqa/test/soap/GeoIpServiceTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.test.soap;
+
+
+import com.lavasoft.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testGeoIp(){
+    String myLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("188.168.136.111");
+    System.out.println(myLocation);
+    Assert.assertEquals(myLocation,"<GeoIP><Country>RU</Country><State>23</State></GeoIP>");
+  }
+}
